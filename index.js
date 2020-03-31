69dd38e55cb871cfea7521b294c54e49dbd28e86
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-03-31 15:09:43 +0300

    v1.0

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var newPlayer = null;
 
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/start.html');
 });
 
 
@@ -29,33 +29,68 @@ app.post('/start', function(req, res){
 
 
 io.on('connection', function(socket){
-	console.log('a user connected');
+	console.log(newPlayer.name + " connected");
 	players.push({
 		id: socket.id,
 		pos: {x: 0, y: 0},
-		speed: 0.4,
+		speed: 0.3,
 		name: newPlayer.name,
-		color: newPlayer.color
+		color: newPlayer.color,
+		score: 0,
+		bullets: 3
 	});
 	socket.emit("player-connected", players[players.length - 1]);
 	io.emit("game-log", newPlayer.name + " connected");
 
 	socket.on("update", (data) => {
+
 		players = players.map(player => {
-			if (player.id == data.id) player = data;
+			if (player.id == data.id) { 
+				player.pos = data.pos;
+				player.speed = data.speed;
+				player.bullets = data.bullets; 
+			}
 			return player;
 		});
-		
+
 		bullets = bullets.map((bul) => {
 			bul.pos.x += bul.speed * bul.cos;
 			bul.pos.y += bul.speed * bul.sin;
 			return bul;
 		});
 
+		var scoresFor = [];
+
 		bullets = bullets.filter((bul) => {
-			if (bul.pos.x + 15 > 800 || bul.pos.x < 0) return false;
+			if (bul.pos.x + 15 > 600 || bul.pos.x < 0) return false;
 			if (bul.pos.y + 15 > 500 || bul.pos.y < 0) return false;
+
+			var isCollide = false;
+			players.forEach((player) => {
+				if ((player.pos.x < bul.pos.x + 15) && (player.pos.x + 40 > bul.pos.x)
+				&& 	(player.pos.y < bul.pos.y + 15) && (player.pos.y + 40 > bul.pos.y)
+				&&  (player.id != bul.id))
+				{
+					isCollide = true;
+					return;
+				}
+			});
+			if (isCollide) {
+				scoresFor.push(bul.id);
+				return false;
+			}
+
 			return true;
+		});
+
+		players = players.map((p) => {
+			if (scoresFor.indexOf(p.id) != -1) {
+				// console.log(p.score);
+				p.score++;
+				p.bullets++;
+				io.emit("game-log", p.name + " score is " + p.score);
+			}
+			return p;
 		});
 
 		io.emit("someone-movement", {players: players, bullets: bullets});
